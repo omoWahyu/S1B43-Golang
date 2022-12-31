0653293c76f6a768ebca47cd5a4cfede465bf278
hyujisf <canqalloid.me@gmail.com>
2022-12-31 14:46:55 +0700

    create data input to memory and append data toProjects data

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -24,7 +25,7 @@ var Data = map[string]interface{}{
 // 	lebar() float64
 // }
 
-type Project struct {
+type dataProject struct {
 	Name        string
 	Start       string
 	End         string
@@ -33,15 +34,15 @@ type Project struct {
 	Duration    string
 }
 
-var Projects = []Project{
-	{
-		Name:        "Dumbways Mobile App 2022",
-		Start:       "1 Des 2022",
-		End:         "9 Des 2022",
-		Duration:    " 1 Minggu",
-		Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		Tech:        []string{"nodejs", "nextjs", "reactjs", "typescript"},
-	},
+var Projects = []dataProject{
+	// {
+	// 	Name:        "Dumbways Mobile App 2022",
+	// 	Start:       "1 Des 2022",
+	// 	End:         "9 Des 2022",
+	// 	Duration:    " 1 Minggu",
+	// 	Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	// 	Tech:        []string{"nodejs", "nextjs", "reactjs", "typescript"},
+	// },
 }
 
 func main() {
@@ -50,9 +51,9 @@ func main() {
 	route.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 
 	route.HandleFunc("/", index).Methods("GET")
-	route.HandleFunc("/project", projectForm).Methods("GET")
+	route.HandleFunc("/project", project).Methods("GET")
 	route.HandleFunc("/project/{id}", projectDetail).Methods("GET")
-	route.HandleFunc("/project", projectAdd).Methods("POST")
+	route.HandleFunc("/project", projectPost).Methods("POST")
 	route.HandleFunc("/contact", contactMe).Methods("GET")
 
 	// port := 5000
@@ -80,7 +81,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, respData)
 }
 
-func projectForm(w http.ResponseWriter, r *http.Request) {
+func project(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	var tmpl, err = template.ParseFiles("views/project-add.html")
@@ -116,20 +117,82 @@ func projectDetail(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, respData)
 }
 
-func projectAdd(w http.ResponseWriter, r *http.Request) {
+func projectPost(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// techstack := r.Form["project-tech"]
-	fmt.Println("Name :" + r.PostForm.Get("project-name"))
-	fmt.Println("Start :" + r.PostForm.Get("project-start"))
-	fmt.Println("End :" + r.PostForm.Get("project-end"))
-	fmt.Println("Description :" + r.PostForm.Get("project-description"))
-	fmt.Println("Tech Stack :", r.Form["project-tech"])
+	// fmt.Println("Name :" + r.PostForm.Get("project-name"))
+	// fmt.Println("Start :" + r.PostForm.Get("project-start"))
+	// fmt.Println("End :" + r.PostForm.Get("project-end"))
+	// fmt.Println("Description :" + r.PostForm.Get("project-description"))
+	// fmt.Println("Tech Stack :", r.Form["project-tech"])
 
+	Name := r.PostForm.Get("project-name")
+	Start := r.PostForm.Get("project-start")
+	End := r.PostForm.Get("project-end")
+	Description := r.PostForm.Get("project-description")
+	Tech := r.Form["project-tech"]
+	Duration := getDuration(Start, End)
+
+	var Project = dataProject{
+		Name,
+		Start,
+		End,
+		Description,
+		Tech,
+		Duration,
+	}
+
+	fmt.Println("Project Name : ", Project.Name)
+	fmt.Println("Start Date   : ", Project.Start)
+	fmt.Println("End Date     : ", Project.End)
+	fmt.Println("Duration     : ", Project.Duration)
+	fmt.Println("Description  : ", Project.Description)
+	fmt.Println("Technologies : ", Project.Tech)
+	fmt.Println("================================")
+
+	Projects = append(Projects, Project)
 	http.Redirect(w, r, "/project", http.StatusMovedPermanently)
+}
+
+func getDuration(start, end string) string {
+
+	// Store Date with the Format
+	DataStart, err := time.Parse("2006-01-02", start)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	DataEnd, err := time.Parse("2006-01-02", end)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	// Get data Range
+	DataRange := DataEnd.Sub(DataStart)
+
+	// Calc duration
+	monthRange := int(DataRange.Hours() / (30 * 24))
+	weekRange := int(DataRange.Hours() / (7 * 24))
+	dayRange := int(DataRange.Hours() / 24)
+
+	if monthRange < 0 {
+		return ""
+	}
+	if monthRange > 0 {
+		return "Duration - " + strconv.Itoa(monthRange) + " Month"
+	}
+	if weekRange != 0 {
+		return "Duration - " + strconv.Itoa(weekRange) + " Week Left"
+	}
+	if dayRange != 0 {
+		return "Duration - " + strconv.Itoa(dayRange) + " Day Left"
+	}
+	return "Duration - Today"
 }
 
 func contactMe(w http.ResponseWriter, r *http.Request) {
